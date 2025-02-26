ef651db177009d794297d33595dfed82c5721134
maxwest97 <52849306+maxwest97@users.noreply.github.com>
2025-02-26 16:40:24 -0500

    Add files via upload
    bug fix

--- kak_tools/aii.py
+++ kak_tools/aii.py
@@ -100,6 +100,25 @@ def symp_diag(h):
     h = np.real(h)
     val, vec = eigh_tridiagonal(np.diag(h), np.diag(h, k=1))
     
+    # shuffle eigenvecs
+    sh = np.zeros((2*l, 2*l))
+    for i in range(2*l):
+        if i<l:
+            if np.abs(vec[0,2*i]) < 1e-6:
+                sh[2*i+1,i] = 1
+            else:
+                sh[2*i+0,i] = 1
+        else:
+            if np.abs(vec[0,1 + 2*(i-l)]) < 1e-6:
+                sh[1+2*(i-l),i] = 1
+            else:
+                sh[0 + 2*(i-l),i] = 1
+    
+    
+    #q = sh.T @ vec.T @ q
+    vs = direct_sum((vec@sh)[:l,:l], (vec@sh)[:l,:l])
+    q = vs.T @ q
+    
     return q
 
 
@@ -119,9 +138,11 @@ def aii_kak(v, omega, verify=0):
     d = expm(1j * w1 @ h @ w1.conj().T)
 
     if verify:
+        
+        assert np.allclose(d, np.diag(d.diagonal()), atol=1e-6), np.amax(d - np.diag(d.diagonal()))
         assert np.allclose(h @ omega, omega @ h.T, atol=1e-6), np.amax(np.abs(h @ omega - omega @ h.T))   # omega skew-sym condition
         assert np.allclose(k.T @ omega @ k, omega, atol=1e-6), np.amax(np.abs(k.T @ omega @ k - omega))   # symplectic condition
-        assert np.allclose(w1.T @ omega @ w1, omega, atol=1e-6) # symplectic condition
+        assert np.allclose(w1.T @ omega @ w1, omega, atol=1e-6), np.amax(np.abs(w1.T @ omega @ w1 - omega)) # symplectic condition
 
         #                       k        a       k   = v
         assert np.allclose(w1.conj().T @ d @ (w1 @ k), v, atol=1e-6), (w1.conj().T @ d @ (w1 @ k), v)
@@ -132,15 +153,19 @@ def aii_kak(v, omega, verify=0):
 
 if __name__ == '__main__':
 
-    l = 30
+    l = 3  # note: we appear to start running into numerical instability at l = 38 (that is, 76 x 76 dimensional matrices)
+           # I have previously seen weird behaviour starting at 75 x 75 matrices (e.g. sharp jumps in execution time) and 
+           # suspect there is some low-level funny business going on 
     
     # symp bilinear form
     jn = np.zeros((2*l, 2*l))
     jn[:l,l:] = -np.eye(l)
     jn[l:,:l] = np.eye(l)
     
-    #u = psm('YIZYI')
-    #u = expm(0.001*logm(unitary_group.rvs(2*l)))
     u = unitary_group.rvs(2*l)
     
     k, a, kp = aii_kak(u, jn, verify=1)
+
+    print(k)
+    print(a)
+    print(kp)
